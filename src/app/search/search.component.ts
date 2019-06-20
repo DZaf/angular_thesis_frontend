c48b688cd50a529531b934f69bb02f1eb682e04b
gZogas <zogasgiorgos@windowslive.com>
2019-06-20 14:41:47 +0300

    git search added in search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -12,32 +13,90 @@ export class SearchComponent implements OnInit {
   code = true;
   web_apis = false;
   everywere = false;
-  constructor(private formBuilder: FormBuilder, private data: DataService) {
+  codeResponse: any;
+  apisResponse: any;
+  keyword: String;
+  tags = [];
+  /* edo to mail tou xristi */
+  email: any = localStorage.getItem('email');
+  constructor(private formBuilder: FormBuilder, private data: DataService, private _router: Router) {
 
     this.messageForm = this.formBuilder.group({
-      keyword: ['', Validators.required]
+      keyword: ['', Validators.required],
+      language: ['', Validators.required]
     })
 
 
-   }
- 
-  ngOnInit() {
   }
 
-   onClick(input){
+  ngOnInit() {
 
-    if (input == "code"){
-      this.code = true;this.web_apis = false;this.everywere = false;
+    this.data.Verify()
+      .subscribe(result => { },
+        error => { this._router.navigate(['/login']) });
+
+  }
+
+  onClick(input) {
+
+    if (input == "code") {
+      this.code = true; this.web_apis = false; this.everywere = false;
     }
-    else if (input == "web_apis"){
-      this.code = false;this.web_apis = true;this.everywere = false;
-    }else{
-      this.code = false;this.web_apis = false;this.everywere = true;
+    else if (input == "web_apis") {
+      this.code = false; this.web_apis = true; this.everywere = false;
+      
+    } else {
+      this.code = false; this.web_apis = false; this.everywere = true;
     }
 
 
-   }
-   
-   
+  }
+
+
+
+  onSubmit() {
+
+    if (this.code) {
+      console.log("submiting form for Code search");
+
+      this.keyword = this.messageForm.controls.keyword.value;
+      const language = this.messageForm.controls.language.value;
+
+      this.data.gitsearch(this.keyword,language) 
+    .subscribe(result =>
+      
+      {
+        this.codeResponse=result; 
+        console.log(result);;
+        },
+      error => {
+        //this.errorResponse = error; 
+        console.log(error); 
+      } );
+
+    }
+    else if (this.web_apis) {
+      console.log("submiting form for  Apis search");
+      
+      this.keyword = this.messageForm.controls.keyword.value;
+      console.log(this.keyword);
+
+      this.tags.push(this.keyword);
+
+      this.data.apiSearch(this.email,this.tags).subscribe(result => {
+        this.apisResponse = result;
+        console.log(result);
+      })
+
+    } else {
+      console.log("submiting form for all");
+
+    }
+
+
+  }
+
+
+
 
 }
